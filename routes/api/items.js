788f6f2f0a4d8a592da18e7df4ab308e5e7bc755
Chris Rico <chris.r.rico@gmail.com>
2021-01-09 09:58:50 -0800

    testing out the post request

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,5 +13,16 @@ router.get('/', (req, res) => {
     .then(items => res.json(items))
 });
 
+// @route POST api/items
+// @desc Create A Post
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(items => res.json(item))
+});
+
 
 module.exports = router;
