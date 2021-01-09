becfe0269bfc9b9b1a39751ed80a22e5297d0c78
Chris Rico <chris.r.rico@gmail.com>
2021-01-09 14:44:48 -0800

    create DELETE route for storage

--- routes/api/items.js
+++ routes/api/items.js
@@ -24,15 +24,13 @@ router.post('/', (req, res) => {
     newItem.save().then(item => res.json(item))
 });
 
-// @route POST api/items
-// @desc Create A Post
+// @route DELETE api/items
+// @desc Delete an Item
 // @access Public
-router.post('/', (req, res) => {
-    const newItem = new Item({
-        name: req.body.name
-    });
-
-    newItem.save().then(item => res.json(item))
+router.delete('/:id', (req, res) => {
+   Item.findById(req.params.id)
+   .then(item => item.remove().then(() => res.json({ success: true })))
+   .catch(err => res.status(404).json({ success: false }));
 });
 
 
