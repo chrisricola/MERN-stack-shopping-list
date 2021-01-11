5f9e1cbff0b29d1120fc3daa9f3037a1c739139b
Chris Rico <chris.r.rico@gmail.com>
2021-01-11 13:46:47 -0800

    added the provider via redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import { Provider } from 'react-redux';
+import store from "./store";
 
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <AppNavbar />
       <ShoppingList />
     </div>
+    </Provider>
   );
 }
 
